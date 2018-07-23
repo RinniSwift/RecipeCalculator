add430e685d06e2f273b748e01ff18f9dc7410db
Rinni Swift <rinradaswift@gmail.com>
2018-07-22 22:12:14 -0700

    starting project

---
+++ Recipe-Calculator/Recipe-Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recipe-Calculator
+//
+//  Created by Rinni Swift on 7/22/18.
+//  Copyright © 2018 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
