fc4a4e105542b0b49888fdac97d6b91462e56a3e
Rinni Swift <rinradaswift@gmail.com>
2018-08-18 09:17:35 -0700

    final product without alert message with instructions

--- Recipe-Calculator/Recipe-Calculator/ViewController.swift
+++ Recipe-Calculator/Recipe-Calculator/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class scrollView: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var images:[String] = ["designdocs", "recipecalcdesign1"]
+    var frame = CGRect(x:0,y:0,width:0,height:0)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        pageControl.numberOfPages = images.count
+        for index in 0..<images.count {
+            
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            
+            let imgView = UIImageView(frame: frame)
+            imgView.image = UIImage(named: images[index])
+            self.scrollView.addSubview(imgView)
+            
+        }
+        
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(images.count), height: scrollView.frame.size.height)
+        scrollView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        var pageNum = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControl.currentPage = Int(pageNum)
+    }
 
 }
 
